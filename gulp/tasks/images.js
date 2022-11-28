5cb9f4e5d423144db15feefc93f57c074c9f00c8
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-28 17:57:45 +0300

    created catalog block

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -9,60 +9,60 @@ export const images = () => {
                 message: "Error: <%= error.message %>"
             }))
         )
-        .pipe(app.plugins.newer(app.path.build.images))
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                webp()
-            )
-        )
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                app.gulp.dest(app.path.build.images)
-            )
-        )
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                app.gulp.src(app.path.src.images)
-            )
-        )
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                app.gulp.src(app.path.src.images)
-            )
-        )
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                app.plugins.newer(app.path.build.images)
-            )
-        )
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                imagemin({
-                    progressive: true,
-                    svgoPlugins: [{ removeViewBox: false }],
-                    interlaced: true,
-                    optimizationLevel: 7 // 0 to 7
-                })
-            )
-        )
-        // .pipe(webp())
-        // .pipe(app.gulp.dest(app.path.build.images))
-        // .pipe(app.gulp.src(app.path.src.images))
         // .pipe(app.plugins.newer(app.path.build.images))
-        // .pipe(imagemin({
-        //     progressive: true,
-        //     svgoPlugins: [{
-        //         removeViewBox: false
-        //     }],
-        //     interlaced: true,
-        //     optimizationLevel: 3 // 0 to 7
-        // }))
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         webp()
+        //     )
+        // )
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         app.gulp.dest(app.path.build.images)
+        //     )
+        // )
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         app.gulp.src(app.path.src.images)
+        //     )
+        // )
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         app.gulp.src(app.path.src.images)
+        //     )
+        // )
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         app.plugins.newer(app.path.build.images)
+        //     )
+        // )
+        // .pipe(
+        //     app.plugins.if(
+        //         app.isBuild,
+        //         imagemin({
+        //             progressive: true,
+        //             svgoPlugins: [{ removeViewBox: false }],
+        //             interlaced: true,
+        //             optimizationLevel: 7 // 0 to 7
+        //         })
+        //     )
+        // )
+        .pipe(webp())
+        .pipe(app.gulp.dest(app.path.build.images))
+        .pipe(app.gulp.src(app.path.src.images))
+        .pipe(app.plugins.newer(app.path.build.images))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            interlaced: true,
+            optimizationLevel: 3 // 0 to 7
+        }))
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.gulp.src(app.path.src.svg))
         .pipe(app.gulp.dest(app.path.build.images))
