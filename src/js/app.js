0d29971329b2c677aec992e58cb2b822f2a53361
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-29 23:11:54 +0300

    adaptived

--- src/js/app.js
+++ src/js/app.js
@@ -15,23 +15,33 @@ const swiper = new Swiper('.swiper', {
         speed: 1000
     },
     // Responsive breakpoints
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //     slidesPerView: 2,
-    //     spaceBetween: 20
-    //     },
-    //     // when window width is >= 480px
-    //     480: {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30
-    //     },
-    //     // when window width is >= 640px
-    //     640: {
-    //     slidesPerView: 4,
-    //     spaceBetween: 40
-    //     }
-    // }
+    breakpoints: {
+        1919: {
+        slidesPerView: 6,
+        spaceBetween: 10
+        },
+        1023: {
+        slidesPerView: 3.2,
+        spaceBetween: 10
+        },
+        // when window width is >= 640px
+        833: {
+        slidesPerView: 2.5,
+        spaceBetween: 30
+        },
+        427: {
+        slidesPerView: 1.9,
+        spaceBetween: 10
+        },
+        374: {
+        slidesPerView: 1.2,
+        spaceBetween: 10
+        },
+        359: {
+        slidesPerView: 1.2,
+        spaceBetween: 10
+        }
+    }
 })
 
 
